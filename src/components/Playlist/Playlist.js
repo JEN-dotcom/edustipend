35889fc67d11299918ae61b5fbdf70835c18aeca
John Enyinwa <johnenyinwa@gmail.com>
2023-05-19 11:25:55 +0100

    added "create playlist" in section

--- src/components/Playlist/Playlist.js
+++ src/components/Playlist/Playlist.js
@@ -3,16 +3,19 @@ import './playlist.css'
 
 const Playlist = () => {
     return (
-        <section className='palu'>p
+        <section>
             <div className='playlist'>
 
+            <h1>Make your party fun!</h1>
+            Create your own custom playlist today.
             </div>
-
+           
             <div className='trending'>
                 <div className='trending__container'>
                     <h3>
                     Trending Songs
                     </h3>
+                    
                     <div className='songs__buttons'>
                         <button>Unavailable by <em>Davido</em> <span> + </span></button>
                         <button>Unavailable by <em>Davido</em><span> + </span></button>
